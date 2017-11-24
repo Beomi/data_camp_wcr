56eb4ba327f02792f6e1e64a302fb9e4214b017d
Beomi <latheledusjp@gmail.com>
2017-11-25 00:40:32 +0900

    W2D2: Update Naver Cafe Board

--- w2d2/06_naver_cafe_board.py
+++ w2d2/06_naver_cafe_board.py
@@ -10,9 +10,10 @@ driver.find_element_by_css_selector('#id').send_keys('')
 driver.find_element_by_css_selector('#pw').send_keys('')
 driver.find_element_by_css_selector('#frmNIDLogin > fieldset > input').click()
 
-# 네이버 가입 카페 목록 가져오기
-CAFE_NUM = 25814079
-driver.get(f'http://cafe.naver.com/ArticleList.nhn?search.clubid={CAFE_NUM}')
+CAFE_NUM = 27436155 # 이 카페 넘버는 카페별로 다릅니다.
+MENU_ID = 44 # 이 메뉴 아이디는 카페 내 게시판별로 다릅니다.
+
+driver.get(f'http://cafe.naver.com/ArticleList.nhn?search.clubid={CAFE_NUM}&search.menuid={MENU_ID}')
 
 # iframe 내부로 이동 (driver가 보는 document의 위치 전환)
 # .frame안의 값은 Frame의 id값
@@ -20,6 +21,7 @@ driver.switch_to.frame('cafe_main')
 # 게시판 내 게시글 가져오기
 post_list = driver.find_elements_by_css_selector('.board-list')
 for post in post_list:
-    print(post.text)
+    print('글제목: ', post.text)
+    print('글링크: ', post.find_element_by_css_selector('a').get_attribute("href"))
 
 driver.quit()
